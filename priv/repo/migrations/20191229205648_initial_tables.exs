b3ff6ffead47ec1ff14f2197f5d73fd85e603692
togmund <togmund@gmail.com>
2019-12-29 13:11:41 -0800

    all tables named, now setting up foreign key relationships

--- priv/repo/migrations/20191229205648_initial_tables.exs
+++ priv/repo/migrations/20191229205648_initial_tables.exs
@@ -2,43 +2,108 @@ defmodule PhoenixReactPlayground.Repo.Migrations.InitialTables do
   use Ecto.Migration
 
   def change do
+    # People
     create table(:users) do
       add :name, :string
 
       timestamps()
     end
 
-    create table(:languages) do
+    create table(:bands) do
       add :name, :string
 
       timestamps()
     end
 
-    create table(:languages) do
+    # Decks
+    create table(:decks) do
       add :name, :string
 
       timestamps()
     end
 
-    create table(:languages) do
+    create table(:deck_cards) do
       add :name, :string
 
       timestamps()
     end
 
-    create table(:languages) do
+    create table(:cards) do
       add :name, :string
 
       timestamps()
     end
 
-    create table(:languages) do
+    # Hexes
+    create table(:hexes) do
       add :name, :string
 
       timestamps()
     end
 
-    create table(:languages) do
+    create table(:hex_users) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:regions) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:region_biomes) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:biomes) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    # Quests
+    create table(:quests) do
+      add :name, :string
+      add :user_quest_id, references("user_quests")
+
+      timestamps()
+    end
+
+    create table(:user_quests) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:card_quests) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:band_quests) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:hex_quests) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:region_quests) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:biome_quests) do
       add :name, :string
 
       timestamps()
