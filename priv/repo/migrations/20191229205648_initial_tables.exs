8176face9133f832819b92c11b492d1a61e4f8af
togmund <togmund@gmail.com>
2019-12-29 12:58:25 -0800

    layout migration for new tables

---
+++ priv/repo/migrations/20191229205648_initial_tables.exs
@@ -0,0 +1,47 @@
+defmodule PhoenixReactPlayground.Repo.Migrations.InitialTables do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:languages) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end
