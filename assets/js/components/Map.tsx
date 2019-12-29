ba55e8578022894d66dd79d91802924c5ad74052
dennis-yin <dennis.yin26@gmail.com>
2019-12-29 15:53:56 -0800

    tune grid parameters

--- assets/js/components/Map.tsx
+++ assets/js/components/Map.tsx
@@ -12,13 +12,18 @@ import {
 import Card from './Card';
 import '../../css/Map.css';
 
-const hexagons = GridGenerator.rectangle(60, 60);
+const hexagons = GridGenerator.orientedRectangle(5, 5);
 
 const Map = () => (
   <main>
     <div className={'hex-map'}>
-      <HexGrid width={1500} height={950} viewBox={'50 100 100 100'}>
-        <Layout size={{ x: 6, y: 6 }}>
+      <HexGrid width={1500} height={950} viewBox={'0 0 100 100'}>
+        <Layout
+          size={{ x: 6, y: 6 }}
+          flat="true"
+          spacing="1.005"
+          origin={{ x: 0, y: 0 }}
+        >
           {hexagons.map((hex: any, i: number) => (
             <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />
           ))}
