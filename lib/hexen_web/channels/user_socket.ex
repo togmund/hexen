8972db09965bdc0f90abdadf5a98c301b131b7f9
Dennis Yin <dennis.yin26@gmail.com>
2019-12-30 16:42:46 -0800

    work on adding counter to hexes, start setting up websocket connection

--- lib/hexen_web/channels/user_socket.ex
+++ lib/hexen_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule HexenWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", HexenWeb.RoomChannel
+  channel "game:*", HexenWeb.GameChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
