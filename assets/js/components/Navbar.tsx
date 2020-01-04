f7c35eab7173ee8addf54fc7bdeffafa73e664d1
dennis-yin <dennis.yin26@gmail.com>
2020-01-04 11:29:00 -0800

    add placeholder info to navbar, some styling, new font

--- assets/js/components/Navbar.tsx
+++ assets/js/components/Navbar.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
 import '../../css/Navbar.css';
 
-const Navbar = () => (
-  <div>
-    <h1>Game info goes here</h1>
-  </div>
-);
+const Navbar = () => {
+  const CARDS_IN_DECK = 40;
+  const ATTACK_CARDS = 15;
+  const GATHER_CARDS = 10;
+  const MOVE_CARDS = 15;
+  const TIMER = 30;
+  const PLAYERS = 4;
+
+  return (
+    <div className={'navbar'}>
+      <img
+        src="https://s.put.re/cH2D7Qq6.png"
+        alt="deck-icon"
+        height="40"
+        width="30"
+      ></img>
+      <p className={'cards-remaining'}>{CARDS_IN_DECK}</p>
+      <p className={'attack-cards-remaining'}>{ATTACK_CARDS}</p>
+      <p className={'gather-cards-remaining'}>{GATHER_CARDS}</p>
+      <p className={'move-cards-remaining'}>{MOVE_CARDS}</p>
+      <p className={'turn-timer'}>{TIMER}</p>
+      <p className={'num-players'}>{PLAYERS}</p>
+    </div>
+  );
+};
 
 export default Navbar;
