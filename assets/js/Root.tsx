e889f1b52735a3d44d35626f8c3a46daf5798639
togmund <togmund@gmail.com>
2020-01-13 14:46:05 -0800

    resized hexes, recreated seeeds

--- assets/js/Root.tsx
+++ assets/js/Root.tsx
@@ -5,7 +5,6 @@ import Sidebar from './components/Sidebar';
 import Map from './components/Map';
 import Hand from './components/Hand';
 import useHexData from './hooks/useHexData';
-import socketHandler from './helpers/socket_handler';
 import '../css/Root.css';
 
 const Root: React.FC = () => {
