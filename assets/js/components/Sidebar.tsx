5c016681b3bd2a7052ff7abe627ca40f760ea6d1
dennis-yin <dennis.yin26@gmail.com>
2020-01-14 09:52:08 -0800

    list other players on hex

--- assets/js/components/Sidebar.tsx
+++ assets/js/components/Sidebar.tsx
@@ -27,9 +27,14 @@ const Sidebar = (props: { state: any; targetHex: any; targetUser: any }) => {
           <p>{state.tile.controlled_by}</p>
           <p>{state.tile.structure}</p>
         </div>
+        <div className={'players-on-hex'}>
+          <ul>
+            {state.tile.players.map((player: any) => {
+              <li>{player.name}</li>;
+            })}
+          </ul>
+        </div>
       </div>
-      {/* Turn timer */}
-      {/* <p>30</p> */}
       <Deck state={state} />
     </nav>
   );
